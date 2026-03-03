938d0c0f80b06b83dcdc4ed5e586a04b5e27ef6b
настя <danilichevanastya@gmail.com>
2026-03-03 20:29:41 +0300

    Add processing module with filter_by_state and sort_by_date functions

---
+++ README.md
@@ -0,0 +1,29 @@
+# Проект home-work
+
+## Описание
+Проект для обработки данных о банковских операциях. Содержит функции фильтрации и сортировки операций.
+
+## Установка
+1. Клонируйте репозиторий:
+```bash
+git clone https://github.com/ВАШ_АККАУНТ/home-work.git
+cd home-work
+
+## ШАГ 7: Отправка изменений на GitHub
+
+В терминале выполните:
+
+```bash
+# Проверим, что мы в нужной ветке
+git branch
+
+# Должна быть активна ветка feature/add-processing-module (отмечена *)
+
+# Добавляем новые файлы
+git add README.md src/processing.py
+
+# Создаем коммит
+git commit -m "Add processing module with filter_by_state and sort_by_date functions"
+
+# Отправляем на GitHub
+git push origin feature/add-processing-module
---
+++ test_processing.py
@@ -0,0 +1,33 @@
+from src.processing import filter_by_state
+
+operations = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
+
+# Получить все выполненные операции
+executed = filter_by_state(operations)
+print(executed)
+
+# Получить все отмененные операции
+canceled = filter_by_state(operations, 'CANCELED')
+print(canceled)
+
+from src.processing import sort_by_date
+
+operations = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
+
+# Сортировка по убыванию (сначала новые)
+sorted_desc = sort_by_date(operations)
+print(sorted_desc)
+
+# Сортировка по возрастанию (сначала старые)
+sorted_asc = sort_by_date(operations, reverse=False)
+print(sorted_asc)
